95ea5a930aa44c44fb029ccb662f4d33da14f701
Xiaonin Li <lixiaonibme@gmail.com>
2016-03-19 17:27:17 -0400

    Initial Commit

---
+++ QuickLocationService/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QuickLocationService
+//
+//  Created by Xiaonin Li on 3/19/16.
+//  Copyright © 2016 KingCatTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QuickLocationService/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QuickLocationService
+//
+//  Created by Xiaonin Li on 3/19/16.
+//  Copyright © 2016 KingCatTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
